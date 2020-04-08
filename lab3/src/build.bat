c54ecc724f1782e78423eb0ddaeed8777696a5b9
Maxim Sednev <darkfreider@gmail.com>
2020-04-08 19:39:21 +0300

    lab3 initial commit

---
+++ lab3/src/build.bat
@@ -0,0 +1,11 @@
+@echo off
+
+set common_compiler_flags=-nologo -Zi
+
+mkdir ..\build
+pushd ..\build
+
+cl %common_compiler_flags% -Zi ..\src\win32_server.cpp
+cl %common_compiler_flags% -Zi ..\src\win32_client.cpp
+
+popd
---
+++ lab3/src/win32_client.cpp
@@ -0,0 +1,9 @@
+
+#include <stdio.h>
+
+
+int main(void)
+{
+	printf("hello from client\n");
+	return (0);
+}
