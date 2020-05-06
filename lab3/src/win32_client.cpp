f0a88b311c3c6128dd04fb3a1a767f5ae47b2ce5
Maxim Sednev <darkfreider@gmail.com>
2020-05-06 11:23:18 +0300

    lab3 for win32 done

--- lab3/src/win32_client.cpp
+++ lab3/src/win32_client.cpp
@@ -1,40 +1,61 @@
 
 #include <Windows.h>
-#include <stdio.h>
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(void)
 {
-	HANDLE pipe_handle = CreateFileA(TEXT("\\\\.\\pipe\\lab3_pipe"), GENERIC_READ, FILE_SHARE_READ, 0, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0);
-
-	if (pipe_handle != INVALID_HANDLE_VALUE)
+	HANDLE mutex = OpenSemaphoreA(EVENT_ALL_ACCESS, 0, "lab3_mutex");
+	if (!mutex)
 	{
-		HANDLE client_semaphore = CreateSemaphoreA(0, 0, 1, "lab3_client_semaphore");
-		if (client_semaphore != 0)
-		{
-			char input_buffer[512] = {};
-
-			for (;;)
-			{
-				HANDLE server_semaphore = OpenSemaphoreA(SYNCHRONIZE, FALSE, "lab3_server_semaphore");
-				ReleaseSemaphore(server_semaphore, 1, 0);
-				CloseHandle(server_semaphore);
-
-				printf("waiting on a client semaphore\n");
-				WaitForSingleObject(client_semaphore, INFINITE);
-
-				DWORD bytes_read;
-				int string_size = 0;
-				ReadFile(pipe_handle, &string_size, sizeof(string_size), &bytes_read, 0);
-				ReadFile(pipe_handle, input_buffer, string_size, &bytes_read, 0);
-				input_buffer[string_size] = 0;
-				printf("%s", input_buffer);
-			}
-
-	
-		}
+		exit(1);
 	}
 
+	HANDLE str_present = OpenSemaphoreA(EVENT_ALL_ACCESS, 0, "lab3_str_present");
+	if (!str_present)
+	{
+		CloseHandle(mutex);
+		exit(1);
+	}
+
+	HANDLE str_not_present = OpenSemaphoreA(EVENT_ALL_ACCESS, 0, "lab3_str_not_present");
+	if (!str_not_present)
+	{
+		CloseHandle(mutex);
+		CloseHandle(str_present);
+		exit(1);
+	}
+
+	HANDLE file_mapping = OpenFileMapping(FILE_MAP_ALL_ACCESS, FALSE, "FMAP");
+	if (file_mapping)
+	{
+		char *shared_data_buffer = (char *)MapViewOfFile(file_mapping, FILE_MAP_ALL_ACCESS, 0, 0, 1024);
+		if (shared_data_buffer)
+		{
+			for (;;)
+			{
+				WaitForSingleObject(str_present, INFINITE);
+				WaitForSingleObject(mutex, INFINITE);
+
+				int len = strlen(shared_data_buffer);
+				printf("%d: %s", len, shared_data_buffer);
+
+				ReleaseSemaphore(mutex, 1, 0);
+				ReleaseSemaphore(str_not_present, 1, 0);
+			}
+
+			UnmapViewOfFile(shared_data_buffer);
+		}
+
+		CloseHandle(file_mapping);
+	}
+
+
+	CloseHandle(mutex);
+	CloseHandle(str_present);
+	CloseHandle(str_not_present);
 
 	return (0);
 }
--- lab3/src/win32_server.cpp
+++ lab3/src/win32_server.cpp
@@ -1,52 +1,75 @@
 
 #include <Windows.h>
 
+#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 
 int main(void)
 {
-	HANDLE pipe_handle = CreateNamedPipeA(TEXT("\\\\.\\pipe\\lab3_pipe"), PIPE_ACCESS_OUTBOUND, PIPE_TYPE_BYTE|PIPE_WAIT, 1, 1024, 1024, 0, 0);
-	if (pipe_handle != INVALID_HANDLE_VALUE)
+	
+	HANDLE mutex = CreateSemaphoreA(0, 1, 1, "lab3_mutex");
+	if (GetLastError() == ERROR_INVALID_HANDLE)
 	{
-		HANDLE server_semaphore = CreateSemaphoreA(0, 0, 1, "lab3_server_semaphore");
-		if (server_semaphore != 0)
-		{
-			printf("waiting for a client to connect\n");
-			BOOL connected = ConnectNamedPipe(pipe_handle, 0) ? TRUE : (GetLastError() == ERROR_PIPE_CONNECTED);
-			if (connected)
-			{
-				char input_buffer[512] = {};
+		exit(1);
+	}
 
+	HANDLE str_present = CreateSemaphoreA(0, 0, 1, "lab3_str_present");
+	if (GetLastError() == ERROR_INVALID_HANDLE)
+	{
+		CloseHandle(mutex);
+		exit(1);
+	}
+
+	HANDLE str_not_present = CreateSemaphoreA(0, 1, 1, "lab3_str_not_present");
+	if (GetLastError() == ERROR_INVALID_HANDLE)
+	{
+		CloseHandle(mutex);
+		CloseHandle(str_present);
+		exit(1);
+	}
+
+	HANDLE file_mapping = CreateFileMapping(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, 0, 1024, "FMAP");
+	if (file_mapping)
+	{
+		char *shared_data_buffer = (char *)MapViewOfFile(file_mapping, FILE_MAP_ALL_ACCESS, 0, 0, 1024);
+		if (shared_data_buffer)
+		{
+			PROCESS_INFORMATION process_info = {};
+			STARTUPINFOA startup_info = {};
+			startup_info.cb = sizeof(startup_info);
+			if (CreateProcessA("win32_client.exe", 0, 0, 0, FALSE, NORMAL_PRIORITY_CLASS, 0, 0, &startup_info, &process_info))
+			{
+				char text_buffer[512];
 				for (;;)
 				{
-					printf("waiting on a server semaphore\n");
-					WaitForSingleObject(server_semaphore, INFINITE);
-
-					printf("enter data: ");
-					int msg_len = scanf("%512s", input_buffer);
-					if (msg_len > 0)
+					char *res = fgets(text_buffer, sizeof(text_buffer), stdin);
+					if (!res)
 					{
-						DWORD bytes_written = 0;
-						WriteFile(pipe_handle, input_buffer, msg_len, &bytes_written, 0);
-
-						HANDLE client_semaphore = OpenSemaphoreA(SYNCHRONIZE, FALSE, "lab3_client_semaphore");
-						ReleaseSemaphore(client_semaphore, 1, 0);
-						CloseHandle(client_semaphore);
-					}
-					else
-					{
-						FlushFileBuffers(pipe_handle);
-						DisconnectNamedPipe(pipe_handle);
 						break;
 					}
+
+					WaitForSingleObject(str_not_present, INFINITE);
+					WaitForSingleObject(mutex, INFINITE);
+
+					CopyMemory(shared_data_buffer, text_buffer, 512);
+
+					ReleaseSemaphore(mutex, 1, 0);
+					ReleaseSemaphore(str_present, 1, 0);
 				}
 			}
+
+			UnmapViewOfFile(shared_data_buffer);
 		}
 
-		CloseHandle(server_semaphore);
-		CloseHandle(pipe_handle);
+		CloseHandle(file_mapping);
 	}
+	
+
+	CloseHandle(mutex);
+	CloseHandle(str_present);
+	CloseHandle(str_not_present);
 
 	return (0);
 }
